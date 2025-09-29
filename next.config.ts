48a9eb8c866f0a1abec984acc3ebbb58d1645304
trevorriggle <155181481+trevorriggle@users.noreply.github.com>
2025-09-29 10:31:26 -0400

    1.0.2

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  experimental: { typedRoutes: true }
+  experimental: {
+    typedRoutes: true
+  }
 };
 
 export default nextConfig;
